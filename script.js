04a156d11bf06266a7754486a8ac6ff96c451456
claytec <cleitonleitesantos20@gmail.com>
2026-03-14 15:54:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('meuQuestionario').addEventListener('submit', function(event) {
+    // Você pode fazer validações extras aqui antes do envio
+    console.log("Formulário pronto para ser enviado!");
+});
